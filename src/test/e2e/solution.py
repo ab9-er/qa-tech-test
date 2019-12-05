c797df9d71a1dc59a3a4a2e1426909ef1351a097
Abhinav <abhinav.gamer@gmail.com>
2019-12-05 12:06:10 +0000

    Add new debug steps

--- src/test/e2e/solution.py
+++ src/test/e2e/solution.py
@@ -1,14 +1,14 @@
-from selenium import webdriver   
+from selenium import webdriver
 
 
 def solve_challenge():
     driver = webdriver.Chrome()
 
-    driver.maximize_window()                                                                     
+    driver.maximize_window()
 
-    driver.get("http://localhost:3000")                                                          
+    driver.get("http://localhost")
 
-    driver.find_element_by_css_selector("[data-test-id='render-challenge']").click() 
+    driver.find_element_by_css_selector("[data-test-id='render-challenge']").click()
 
     rows = driver.find_elements_by_css_selector(".challenge tr")
 
@@ -18,11 +18,21 @@ def solve_challenge():
         elem_list = list(map(lambda x: int(x.text), elems))
 
         for _i in range(len(elem_list)):
-            if sum(elem_list[:_i-1]) == sum(elem_list[_i:]):
+            print("\nelem_list[] {}".format(elem_list))
+            print("[_i] {}".format(_i))
+            print("elem_list[_i] {}".format(elem_list[_i]))
+            print("elem_list[:_i] {} {}".format(str(elem_list[:_i]), str(sum(elem_list[:_i]))))
+            print("elem_list[_i+1:] {} {}".format(str(elem_list[_i+1:]), str(sum(elem_list[_i+1:]))))
+
+            if sum(elem_list[:_i]) == sum(elem_list[_i+1:]):  # Add try block to catch exception for last element
                 INDEX_FOUND = True
+                print("Balanced")
                 break
-        
-        print(_i - 1) if INDEX_FOUND else print(None)
+            else:
+                print("Unbalanced")
+
+        print(_i) if INDEX_FOUND else print(None)
+        print("\n" + "-" * 30 + "\n")
 
 if __name__ == "__main__":
     solve_challenge()
